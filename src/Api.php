3390cfacaf1c4f74e46bcc3389dfcf94b4b0f389
Angel S. Moreno <363479+angelxmoreno@users.noreply.github.com>
2018-09-14 01:25:11 -0400

    Added cache integration (#16)

--- src/Api.php
+++ src/Api.php
@@ -2,6 +2,9 @@
 
 namespace Axm\DobaApi;
 
+use Cake\Utility\Hash;
+use Psr\SimpleCache\CacheInterface;
+
 /**
  * Class Api
  * @package Axm\DobaApi
@@ -20,6 +23,9 @@ namespace Axm\DobaApi;
  */
 class Api
 {
+    public const CACHE_TTL_KEY = 'ttl';
+    public const DEFAULT_CACHE_TTL = 300;
+
     public const PRODUCT_METHODS = [
         'getSuppliers',
         'searchCatalog',
@@ -48,6 +54,11 @@ class Api
     protected $product_client;
 
     /**
+     * @var CacheInterface
+     */
+    protected $cache;
+
+    /**
      * Api constructor.
      * @param Client $order_client
      * @param Client $product_client
@@ -58,17 +69,78 @@ class Api
         $this->product_client = $product_client;
     }
 
+    /**
+     * @param string $method
+     * @param array $arguments
+     * @return array
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
     public function __call($method, $arguments)
     {
         if (!in_array($method, self::ORDER_METHODS) && !in_array($method, self::PRODUCT_METHODS)) {
             throw new \BadMethodCallException("Method {$method} does not exist");
         }
 
+        return $this->callClient($method, $arguments);
+    }
+
+    /**
+     * @param string $method
+     * @param array $arguments
+     * @return array
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    protected function callClient(string $method, array $arguments) : array
+    {
+        $cache_ttl = Hash::get($arguments, self::CACHE_TTL_KEY, self::DEFAULT_CACHE_TTL);
+
+        return ($this->cache && $cache_ttl)
+            ? $this->callCached($method, $arguments, $cache_ttl)
+            : $this->callRaw($method, $arguments);
+    }
+
+    protected function callRaw(string $method, array $arguments) : array
+    {
         $client = in_array($method, self::ORDER_METHODS)
             ? $this->order_client
             : $this->product_client;
+
         array_unshift($arguments, $method);
 
         return call_user_func_array([$client, 'call'], $arguments);
+    }
+
+    /**
+     * @param string $method
+     * @param array $arguments
+     * @param int $ttl
+     * @return array
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    protected function callCached(string $method, array $arguments, int $ttl) : array
+    {
+        $cache_key = $this->getCacheKey($method, $arguments);
+
+        if ($this->cache->has($cache_key)) {
+            return $this->cache->get($cache_key);
+        }
+
+        $response = $this->callRaw($method, $arguments);
+        $this->cache->set($cache_key, $response, $ttl);
+
+        return $response;
+    }
+
+    /**
+     * @param CacheInterface $cache
+     */
+    public function setCache(CacheInterface $cache) : void
+    {
+        $this->cache = $cache;
+    }
+
+    protected function getCacheKey(string $method, array $arguments) : string
+    {
+        return md5($method . serialize($arguments));
     }
 }
