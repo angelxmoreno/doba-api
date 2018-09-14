b258295c6d6b83e541461fa472377cb96abb1e64
Angel S. Moreno <363479+angelxmoreno@users.noreply.github.com>
2018-09-14 00:03:36 -0400

    Bug/13 (#15)
    * Added failing test to capture bug
    
    * Fixed getWsdlUrl bug
    
    * reverted method visibility

--- tests/Unit/FactorySpec.php
+++ tests/Unit/FactorySpec.php
@@ -5,13 +5,32 @@ namespace Axm\DobaApi\Tests\Unit;
 use Axm\DobaApi\Api;
 use Axm\DobaApi\Client;
 use Axm\DobaApi\Factory;
+use Axm\DobaApi\Tests\TestHelper;
 
 describe(Factory::class, function () {
-    describe('->buildApi()', function () {
+    describe('::buildApi()', function () {
         it('builds an Api object', function () {
             $obj = Factory::buildApi('', '', '');
 
             expect($obj)->toBeAnInstanceOf(Api::class);
+        });
+
+        it('builds ' . Client::class . ' for orders', function () {
+            $obj = Factory::buildApi('', '', '');
+
+            /** @var Client $client */
+            $client = TestHelper::getProperty($obj, 'order_client');
+            expect($client)->toBeAnInstanceOf(Client::class);
+            expect($client->getWsdlUrl())->toBe(Factory::ORDER_WSDL_URL_DEV);
+        });
+
+        it('builds ' . Client::class . ' for products', function () {
+            $obj = Factory::buildApi('', '', '');
+
+            /** @var Client $client */
+            $client = TestHelper::getProperty($obj, 'product_client');
+            expect($client)->toBeAnInstanceOf(Client::class);
+            expect($client->getWsdlUrl())->toBe(Factory::PRODUCT_WSDL_URL_DEV);
         });
     });
     describe('::buildClient()', function () {
